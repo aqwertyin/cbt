183bb07cfff7521d835ae229ae92dba179d0da96
aqwertyin <78072680+aqwertyin@users.noreply.github.com>
2021-02-02 11:11:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 window.onscroll=function(){fn_sticky_nav()};var navbar_container=document.getElementById("nav_container"),sticky=navbar_container.offsetTop;function fn_sticky_nav(){window.pageYOffset>sticky?navbar_container.classList.add("sticky"):navbar_container.classList.remove("sticky")}
-
+function fn_responsive_nav(){var a=document.getElementById("myTopnav");"topnav"===a.className?a.className+=" responsive":a.className="topnav"}
